5fd6fd83b9531efeedff7cb030a52a2ee1bae1c7
novailoveyou <novailoveyoux@gmail.com>
2022-08-06 23:17:50 +0300

    the skeleton of the yew app

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,29 @@
 use yew::prelude::*;
 
+#[function_component(Producer)]
+fn producer() -> Html {
+	html!(
+		<div class="producer">
+			<h3 class="h3">{ "Producer" }</h3>
+		</div>
+	)
+}
+
+#[function_component(Consumer)]
+fn consumer() -> Html {
+	html!(
+		<div class="consumer">
+			<h3 class="h3">{ "Consumer" }</h3>
+		</div>
+	)
+}
+
 #[function_component(App)]
 fn app() -> Html {
 	html!(
-		<div>
-			Hi!!!
+		<div class="grid">
+			<Producer />
+			<Consumer />
 		</div>
 	)
 }
