f9b63be7ddd53a09ad2f964bf2308af61ae063c8
novailoveyou <novailoveyoux@gmail.com>
2022-08-06 19:07:16 +0300

    basic yew app structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html {
+	html!(
+		<div>
+			Hi!!!
+		</div>
+	)
+}
+
+fn main(){
+	yew::start_app::<App>();
 }
